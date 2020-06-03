f3fd8d1758520ddafc1448aa8addeaf6320366d4
PARDEEP KAUR <pkaur84@aol.com>
2020-06-03 19:43:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,6 +34,10 @@ def initialize(name, grade, id=nil)
       INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
   
   def self.create(name:, grade:)
     student = Student.new(name,grade)
