06aae5717c2d03459da02d2658c0a56dee445f0d
PARDEEP KAUR <pkaur84@aol.com>
2020-06-03 19:06:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ def initialize(name, grade, id=nil)
  
   end
   
-  def self.create(name, grade)
+  def self.create(name:, grade:)
     student = Student.new(name,grade)
     student.save
     student
